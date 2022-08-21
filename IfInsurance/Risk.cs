cc68ad00e3312e6efa462ac0a4b5f14e912ebeeb
Liga <ligacepurniece@gmail.com>
2022-08-21 11:52:01 +0300

    Add interfaces and struct

---
+++ IfInsurance/Risk.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfInsurance
+{
+    public struct Risk
+    {
+        /// <summary>
+        /// Unique name of the risk
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Risk yearly price
+        /// </summary>
+        public decimal YearlyPrice { get; set; }
+    }
+}
