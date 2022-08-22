dd2cd6d8f56260191ec1bcfd583a2fd6ee15cd69
Liga <ligacepurniece@gmail.com>
2022-08-22 20:42:39 +0300

    Add classes

---
+++ IfInsurance/InsuranceCompany.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfInsurance
+{
+    public class InsuranceCompany : IInsuranceCompany
+    {
+        public string Name { get; }
+
+        public IList<Risk> AvailableRisks { get; set; }
+
+        public void AddRisk(string nameOfInsuredObject, Risk risk, DateTime validFrom)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPolicy GetPolicy(string nameOfInsuredObject, DateTime effectiveDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPolicy SellPolicy(string nameOfInsuredObject, DateTime validFrom, short validMonths, IList<Risk> selectedRisks)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IfInsurance/Policy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfInsurance
+{
+    public class Policy : IPolicy
+    {
+        public string NameOfInsuredObject { get; }
+
+        public DateTime ValidFrom { get; }
+
+        public DateTime ValidTill { get; }
+
+        public decimal Premium { get; }
+
+        public IList<Risk> InsuredRisks { get; }
+    }
+}
