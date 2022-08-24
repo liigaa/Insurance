9fba4fe93b5ede895f082d1c456edf97ef5d2fd0
Liga <ligacepurniece@gmail.com>
2022-08-24 07:25:51 +0300

    Test commit

--- InsuranceTest/InsuranceTest.cs
+++ InsuranceTest/InsuranceTest.cs
@@ -2,6 +2,7 @@ using NUnit.Framework;
 using IfInsurance;
 using FluentAssertions;
 using System.Collections.Generic;
+using System;
 
 namespace InsuranceTest
 {
@@ -9,6 +10,7 @@ namespace InsuranceTest
     {
         private IInsuranceCompany _insuranceCompany;
         private IList<Risk> _availableRisks;
+        private IList<Policy> _availablePolicies;
         [SetUp]
         public void Setup()
         {
@@ -19,13 +21,27 @@ namespace InsuranceTest
                 new Risk("Home", 500)
             };
             _insuranceCompany = new InsuranceCompany("If", _availableRisks);
+
+           
+            _availablePolicies = new List<Policy>
+            {
+                new Policy("Car", new DateTime(2022,9,1), new DateTime(2023,1,1), 400, _availableRisks)
+            };
         }
 
         [Test]
-        public void CompanyAddAvailableRisks()
+        public void CompanyAddAvailableRisk()
         {
             _availableRisks.Add(new Risk("Injuries", 150));
             _insuranceCompany.AvailableRisks.Should().BeEquivalentTo(_availableRisks);
         }
+
+        [Test]
+        public void CompanyAddPolicy()
+        {
+            _availablePolicies.Add(new Policy("House", new DateTime(2022, 10, 1), new DateTime(2023, 1, 1), 600, _availableRisks));
+           // _insuranceCompany
+        }
+
     }
 }
