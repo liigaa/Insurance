67281dfac33d1c899eb6928d83aa725c7b0d8e2a
Liga <ligacepurniece@gmail.com>
2022-08-25 13:32:41 +0300

    Add Date Exaption

---
+++ IfInsurance/Exceptions/DateException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfInsurance.Exceptions
+{
+    internal class DateException : Exception
+    {
+        public DateException() : base("Cannot be policy effective date in past")
+        {
+        }
+
+        public DateException(string message) : base(message)
+        {
+        }
+
+        public DateException(string message, Exception exception) : base(message, exception)
+        {
+        }
+    }
+}
